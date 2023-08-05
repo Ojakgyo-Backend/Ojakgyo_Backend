f92cbe374b775aaf63d6956ddecfcb6c9d78e616
yujeongkwon <yoojeongkwon@naver.com>
2023-08-05 14:16:06 +0900

    Correct() : spirng security 기능 구현에 따라 user MVC 수정

--- src/main/java/Ojakgyo/com/example/Ojakgyo/controller/UserSignupController.java
+++ src/main/java/Ojakgyo/com/example/Ojakgyo/controller/UserSignupController.java
@@ -4,6 +4,9 @@ import Ojakgyo.com.example.Ojakgyo.dto.UserSignupDto;
 import Ojakgyo.com.example.Ojakgyo.service.SignupService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
@@ -12,22 +15,22 @@ import java.util.Map;
 @Slf4j
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("user/sign-up")
+@RequestMapping("/sign-up")
 public class UserSignupController {
 
     private final SignupService signupService;
-
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
     /*
     가능하다면 sms 인증 추가 + 비밀번호 암호화 추가
     priate final SmsService smsService
     * */
 
     @PostMapping
-    public Object register(@ModelAttribute UserSignupDto request) throws IOException {
+    public Object register(@RequestBody UserSignupDto request) throws IOException {
 
         try {
             signupService.checkPhone(request.getPhone());
-            request.setPassword(request.getPassword());
+            request.setPassword(bCryptPasswordEncoder.encode(request.getPassword()));
             signupService.signup(request);
             return Map.of("result", "성공");
         } catch (Exception e) {
