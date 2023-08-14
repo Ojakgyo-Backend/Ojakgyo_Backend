18c8be5df77f4ea61b77177be6d730c14412aad2
o-jeong <ljj020304@gmail.com>
2023-08-15 00:24:30 +0900

    fix(UserSignUpDto.java) : 생성자 추가

--- src/main/java/Ojakgyo/com/example/Ojakgyo/dto/UserSignupDto.java
+++ src/main/java/Ojakgyo/com/example/Ojakgyo/dto/UserSignupDto.java
@@ -17,6 +17,10 @@ import javax.validation.constraints.Size;
 @Setter
 @Data
 public class UserSignupDto {
+    public UserSignupDto() {
+
+    }
+
     @Size(min=2, max=100, message = "이메일은 2글자 이상, 100글자 이하로 작성해 주세요.")
     @NotBlank(message = "이메일을 입력해 주세요.")
     @Email(message = "이메일 형식에 맞지 않습니다")
