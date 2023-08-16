753ad3ea0f8be0948193bf024817ee3685795d69
yujeongkwon <yoojeongkwon@naver.com>
2023-08-16 20:16:07 +0900

    Refactor(deal) : 리펙터링

--- src/main/java/Ojakgyo/com/example/Ojakgyo/repository/UserRepository.java
+++ src/main/java/Ojakgyo/com/example/Ojakgyo/repository/UserRepository.java
@@ -3,9 +3,12 @@ package Ojakgyo.com.example.Ojakgyo.repository;
 import Ojakgyo.com.example.Ojakgyo.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
     User findByEmail(String email);
     User findByPhone(String phone);
     User findByNameAndPhone(String name, String phone);
     User findByEmailAndPhone(String email, String phone);
+    Optional<User> findById(Long id);
 }
