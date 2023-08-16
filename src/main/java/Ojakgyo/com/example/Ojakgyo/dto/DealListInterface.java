6111ca7bb90178b4bfd943668590de9ec3bec4dd
yujeongkwon <yoojeongkwon@naver.com>
2023-08-16 23:48:51 +0900

    featuer() : 거래 대상자 조회 기능 구현

---
+++ src/main/java/Ojakgyo/com/example/Ojakgyo/dto/DealListInterface.java
@@ -0,0 +1,12 @@
+package Ojakgyo.com.example.Ojakgyo.dto;
+
+
+import Ojakgyo.com.example.Ojakgyo.domain.DealStatus;
+
+import java.time.LocalDateTime;
+
+public interface DealListInterface {
+     DealStatus getDealStatus();
+     String getItem();
+     LocalDateTime getUpdateAt();
+}
