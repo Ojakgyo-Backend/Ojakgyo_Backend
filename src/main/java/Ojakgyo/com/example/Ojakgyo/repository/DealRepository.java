623b462a78f2682e2f38cc2e4339d3a061f09b0b
yujeongkwon <yoojeongkwon@naver.com>
2023-08-17 20:19:07 +0900

    fix() : null 값 출력 수정

--- src/main/java/Ojakgyo/com/example/Ojakgyo/repository/DealRepository.java
+++ src/main/java/Ojakgyo/com/example/Ojakgyo/repository/DealRepository.java
@@ -3,12 +3,12 @@ package Ojakgyo.com.example.Ojakgyo.repository;
 import Ojakgyo.com.example.Ojakgyo.domain.Deal;
 import Ojakgyo.com.example.Ojakgyo.dto.DealListInterface;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface DealRepository extends JpaRepository<Deal,Long> {
-    @Query(value = "SELECT deal_status, item, update_at FROM deal WHERE seller_id = :id OR buyer_id =:id ", nativeQuery = true)
-    List<DealListInterface> findDealerDealsById(long id);
+//    @Query(value = "SELECT d.deal_status, d.item, d.update_at FROM Deal d WHERE d.seller_id = :id OR d.buyer_id =:id ", nativeQuery = true)
+    List<DealListInterface> findBySellerId(Long id);
+    List<DealListInterface> findByBuyerId(Long id);
     Deal findDealById(long dealId);
 }
