d21a457dc0481d16d765ed22234fd2d33f755a51
yujeongkwon <87534067+yujeongkwon@users.noreply.github.com>
2023-11-20 17:21:26 +0900

    test(BlockChain) : 성공

--- src/main/java/Ojakgyo/com/example/Ojakgyo/BlockChain/RequestBlockChain.java
+++ src/main/java/Ojakgyo/com/example/Ojakgyo/BlockChain/RequestBlockChain.java
@@ -7,18 +7,34 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
+
 
 @RequiredArgsConstructor
 @Service
 public class RequestBlockChain {
-    private final RestTemplate restTemplate;
+    private final RestTemplate restTemplate = new RestTemplate();
 
-    private final String BlockChainServerTest = "ec2-15-164-170-1.ap-northeast-2.compute.amazonaws.com:8081/api/test";
+    private final String BlockChainServer = "http://ec2-15-164-170-1.ap-northeast-2.compute.amazonaws.com:8081/api";
 
-    public BlockChainContract requestBlock(Long dealId) {
-        final HttpHeaders headers = new HttpHeaders(); // 헤더에 key들을 담아준다.
-        final HttpEntity<String> entity = new HttpEntity<>(headers);
-        return restTemplate.exchange(BlockChainServerTest, HttpMethod.GET, entity, BlockChainContract.class, dealId)
+    public BlockChainContract requestBlock(long dealId) {
+        return restTemplate.exchange(createApiUri(dealId),
+                        HttpMethod.GET,
+                        new HttpEntity<>(createHttpHeaders()),
+                        BlockChainContract.class)
                 .getBody();
+    }
+
+    private String createApiUri(final long dealId) {
+        return UriComponentsBuilder.fromUriString(BlockChainServer)
+                .path("/test")
+                .queryParam("dealId",dealId)
+                .encode()
+                .toUriString();
+    }
+
+    private HttpHeaders createHttpHeaders() {
+        final HttpHeaders headers = new HttpHeaders();
+        return headers;
     }
 }
