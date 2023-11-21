df4490975d5afc4dcb0682e48c6b85adc1b2cab6
yujeongkwon <87534067+yujeongkwon@users.noreply.github.com>
2023-11-22 01:00:28 +0900

    feat(ContractController) : 블록체인 연결 api 호출

--- src/main/java/Ojakgyo/com/example/Ojakgyo/BlockChain/RequestBlockChain.java
+++ src/main/java/Ojakgyo/com/example/Ojakgyo/BlockChain/RequestBlockChain.java
@@ -1,6 +1,8 @@
 package Ojakgyo.com.example.Ojakgyo.BlockChain;
 
 import Ojakgyo.com.example.Ojakgyo.dto.BlockChainContract;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
@@ -17,24 +19,50 @@ public class RequestBlockChain {
 
     private final String BlockChainServer = "http://ec2-15-164-170-1.ap-northeast-2.compute.amazonaws.com:8081/api";
 
-    public BlockChainContract requestBlock(long dealId) {
-        return restTemplate.exchange(createApiUri(dealId),
+    public BlockChainContract requestSaveBlock(BlockChainContract request) {
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Content-Type", "application/json");
+
+        try {
+            // 객체를 JSON 문자열로 변환
+            String requestBody = new ObjectMapper().writeValueAsString(request);
+
+            // 헤더와 바디 합치기
+            HttpEntity<String> entity = new HttpEntity<>(requestBody, headers);
+
+            return restTemplate.exchange(createSaveBlockApiUri(),
+                            HttpMethod.POST,
+                            entity,
+                            BlockChainContract.class)
+                    .getBody();
+        } catch (JsonProcessingException e) {
+            // JSON 변환 오류 처리
+            e.printStackTrace();
+            return null; // 또는 예외 처리에 따라 다른 방식으로 처리
+        }
+    }
+
+    private String createSaveBlockApiUri() {
+        return UriComponentsBuilder.fromUriString(BlockChainServer)
+                .path("/blockchain")
+                .encode()
+                .toUriString();
+    }
+
+    public BlockChainContract requestGetBlock(long dealId) {
+        return restTemplate.exchange(createGetBlockApiUri(dealId),
                         HttpMethod.GET,
-                        new HttpEntity<>(createHttpHeaders()),
+                        new HttpEntity<>(new HttpHeaders()),
                         BlockChainContract.class)
                 .getBody();
     }
 
-    private String createApiUri(final long dealId) {
+    private String createGetBlockApiUri(final long dealId) {
         return UriComponentsBuilder.fromUriString(BlockChainServer)
-                .path("/test")
+                .path("/blockchain")
                 .queryParam("dealId",dealId)
                 .encode()
                 .toUriString();
     }
 
-    private HttpHeaders createHttpHeaders() {
-        final HttpHeaders headers = new HttpHeaders();
-        return headers;
-    }
 }
